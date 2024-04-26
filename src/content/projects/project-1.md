0af00c74cbb797aba41f076aeb84558c95d6816a
Michael Yubank <meys98@outlook.com>
2024-04-25 19:31:40 -0600

    First commit
    Primer commit de mi portafolio personal

---
+++ src/content/projects/project-1.md
@@ -0,0 +1,42 @@
+---
+title: 'PokeDex: Pokemon web App'
+description: PokeDex is a Personal project inspired by the Pokédex! Dive into the Pokémon universe through my own version of the Pokédex, created with React and powered by the PokeAPI. 
+publishDate: 'February 2024'
+seo:
+  image:
+    src: '/project-1.png'
+    alt: Project preview
+---
+
+![Project preview](/project-1.png)
+
+
+**Project Overview:**
+Pokedex is a personal web project that unites my passion for programming and the geek world. 
+
+## Objectives
+
+1. Develop an easy to use web application to learn more about pokemon and their abilities.
+2. Use the React framework and improve the handling of states and elements that allow to have a fast and stable web when consuming an api.
+3. Provide me with an educational resource and customized challenges to improve my level of JavaScript and interface development.
+
+## Features
+
+1. **Search Bar By Type:**
+
+- Users are allowed to select the type of item they wish to view.
+- The pokemon that match the selected element will be displayed, even if it has several types.
+
+2. **Grid Cards:**
+
+- The pokemon will be displayed in mini cards which have an image of the pokemon along with their types, colors and number assigned in the official pokedex. 
+
+3. **Card Information:**
+
+- Detailed information about the pokemon is displayed along with a larger view.
+
+## Technology Stack
+
+- Frontend: React ![react_logo](/react.svg) for responsive-platform web app development.
+- Backend: Firebase for real-time
+- Database: Api Pokedex
---
+++ src/data/site-config.ts
@@ -0,0 +1,182 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+    image?: string;
+};
+
+export type Hero = {
+    title?: string;
+    tools?: Image[];
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+export type Subscribe = {
+    title?: string;
+    text?: string;
+    formUrl: string;
+};
+
+export type SiteConfig = {
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    subscribe?: Subscribe;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    title: 'Michael Yubank',
+    subtitle: 'Portfolio and blog by Michael Yubank',
+    description: 'Astro.js and Tailwind CSS',
+    image: {
+        src: '/dante-preview.jpg',
+        alt: 'Dante - Astro.js and Tailwind CSS theme'
+    },
+    headerNavLinks: [
+        {
+            text: 'Home',
+            href: '/',
+            image: '/home.svg'
+        },
+        {
+            text: 'Projects',
+            href: '/projects',
+            image: '/projects.svg'
+        },
+        {
+            text: 'Tags',
+            href: '/tags',
+            image: '/tags.svg'
+        }
+    ],
+    footerNavLinks: [
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        },
+      
+    ],
+    socialLinks: [
+        {
+            text: 'Instagram',
+            href: 'https://instagram.com/',
+            image: '/instagram.svg'
+        },
+        {
+            text: 'Github',
+            href: 'https://github.com/yubank98',
+            image: '/github.svg'
+        },
+        {
+            text: 'Linkedin',
+            href: 'https://www.linkedin.com/in/michael-yubank/',
+            image: '/linkedin.svg'
+        }
+    ],
+    hero: {
+        title: 'Hi There & Welcome to My PortFolio!',
+        tools: [
+            //Java, JavaScript, Typescript, ReactJS, Angular, Git, PHP, Python, .NET Framework, C#, C++, SQL Server, MySQL, Laravel, Node.js, Express.js
+            {
+                src: '/react.svg',
+                alt: 'react.js logo' 
+            },
+            {
+                src: '/java.svg',
+                alt: 'java.js logo'
+            },
+            {
+                src: '/javascript.svg',
+                alt: 'javascript.js logo'
+            },
+            {
+                src: '/typescript.svg',
+                alt: 'typescript.js logo'
+            },
+            {
+                src: '/angular.svg',
+                alt: 'angular.js logo'
+            },
+            {
+                src: '/git.svg',
+                alt: 'git.js logo'
+            },
+            {
+                src: '/php.svg',
+                alt: 'php.js logo'
+            },
+            {
+                src: '/python.svg',
+                alt: 'python.js logo'
+            },
+            {
+                src: '/dotnet.svg',
+                alt: 'dotnet.js logo'
+            },
+            {
+                src: '/csharp.svg',
+                alt: 'csharp.js logo'
+            },
+            {
+                src: '/cplusplus.svg',
+                alt: 'cplusplus.js logo'
+            },
+            {
+                src: '/sqlserver.svg',
+                alt: 'sqlserver.js logo'
+            },
+            {
+                src: '/mysql.svg',
+                alt: 'mysql.js logo'
+            },
+            {
+                src: '/laravel.svg',
+                alt: 'laravel.js logo'
+            },
+            {
+                src: '/nodejs.svg',
+                alt: 'nodejs.js logo'
+            },
+            {
+                src: '/astro.svg',
+                alt: 'astro.js logo'
+            }
+          
+           
+        ],
+        text: "I'm **Michael Yubank**, a Jr development with over 1 year and 6 months of experience working in software development and data analysis. Skilled in problem-solving, teamwork, optimization, and maintenance, empathy, and decision-making,Feel free to explore some of my coding endeavors on <a href='https://github.com/yubank98'>GitHub</a> or follow me on <a href='https://www.linkedin.com/in/michael-yubank/'>Linkedin</a>.",
+        image: {
+            src: '/Profile1.jpg',
+            alt: 'Me in a suit and tie smiling at the camera.'
+        },
+        actions: [
+            {
+                text: 'Get in Touch',
+                href: '/contact'
+            }
+        ]
+    },
+    postsPerPage: 8,
+    projectsPerPage: 8
+};
+
+export default siteConfig;
